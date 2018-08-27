c17de7960e46c008a3ba5631dd3a7041d9a64525
Dmytro Klimchuk <Dmytro.Klimchuk@gmail.com>
2018-08-27 21:22:17 +0300

    fixed mistake with selected item

--- src/components/creditDetail/creditDetail.js
+++ src/components/creditDetail/creditDetail.js
@@ -14,6 +14,8 @@ class CreditDetail extends Component {
 
         let { id } = this.props.match.params;
 
+        let item = this.state.data.filter( (item) => item.id == id)[0];
+
         return (
             <div className="container">
                 <div className="row">
@@ -33,12 +35,12 @@ class CreditDetail extends Component {
                                 <th>Total laan</th>
                             </tr>
                             <tr>
-                                <td>{this.state.data[id].name }</td>
-                                <td>{this.state.data[id].surname }</td>
-                                <td>{this.state.data[id].amount }</td>
-                                <td>{this.state.data[id].term }</td>
-                                <td>{this.state.data[id].rate }</td>
-                                <td>{this.state.data[id].total }</td>
+                                <td>{ item.name }</td>
+                                <td>{ item.surname }</td>
+                                <td>{ item.amount }</td>
+                                <td>{ item.term }</td>
+                                <td>{ item.rate }</td>
+                                <td>{ item.total }</td>
                             </tr>
                         </tbody>
                     </table>
